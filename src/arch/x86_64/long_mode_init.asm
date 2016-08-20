0ab66df9fc37d480b631714c088e23928dd1ae5a
Bernardo Meurer <meurerbernardo@gmail.com>
2016-08-20 13:13:26 -0300

    Added clearing routine

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -2,20 +2,23 @@ global long_mode_start
 
 section .text
 bits 64
-VGA_BUFFER equ 0xb8000
-long_mode_start:
-clear:
-    mov rsi, clr
-    mov rdi, VGA_BUFFER
-    mov ah, 0x02
-loop:
-    lodsb
-    test al,al
-    je end
-    stosw
-    jmp loop
-end:
-    hlt
+    VGA_BUFFER equ 0xb8000
+    long_mode_start:
+        mov rsi, msg
+        mov rdi, VGA_BUFFER
+        mov ax, 0x0720
+        mov rcx, 80
+        rep stosw
+        mov rdi, VGA_BUFFER
+        mov ah, 0x02
+    loop:
+        lodsb
+        test al,al
+        je end
+        stosw
+        jmp loop
+    end:
+        hlt
+
 section .data
-    msg db "Thanks Dan!", 0
-    clr db "                                                                                ", 0
+    msg db "Hey Daniel. Isn't this cool?", 0
