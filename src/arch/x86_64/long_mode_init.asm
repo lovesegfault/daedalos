8043a99f83f53df505eb5ab1e7fe1b26d5f6c38f
Bernardo Meurer <meurerbernardo@gmail.com>
2016-08-20 14:41:32 -0300

    Started implementing C

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -2,23 +2,9 @@ global long_mode_start
 
 section .text
 bits 64
-    VGA_BUFFER equ 0xb8000
     long_mode_start:
-        mov rsi, msg
-        mov rdi, VGA_BUFFER
-        mov ax, 0x0720
-        mov rcx, 80
-        rep stosw
-        mov rdi, VGA_BUFFER
-        mov ah, 0x02
-    loop:
-        lodsb
-        test al,al
-        je end
-        stosw
-        jmp loop
-    end:
+        ; print `OKAY` to screen
+        call kernel_main
+        cli
         hlt
-
-section .data
-    msg db "Hey Daniel. Isn't this cool?", 0
+        
