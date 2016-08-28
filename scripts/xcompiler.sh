3bbe31064bfdec088769d2f8f874bdf710bb6bcc
Bernardo Meurer <meurerbernardo@gmail.com>
2016-08-27 22:46:04 -0300

    Added variables for versions

--- scripts/xcompiler.sh
+++ scripts/xcompiler.sh
@@ -6,6 +6,12 @@ PATH=$PATH:$PREFIX/bin
 CFLAGS="-g -O2"
 CCOUNT=$(nproc --all)
 
+BIN_VER="binutils-2.27"
+GCC_VER="gcc-6.2.0"
+GMP_VER="gmp-6.1.1"
+MPF_VER="mpfr-3.1.4"
+MPC_VER="mpc-1.0.3"
+
 if [ "$(uname)" == "Darwin" ]; then
 	echo "This doesn't work on MacOS yet!"
     exit
@@ -21,36 +27,36 @@ if [[ ! -d xcompiler/src ]]; then
     cd xcompiler/src || exit
 
     echo 'Downloading Sources'
-    wget http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz
-    wget http://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.gz
-    wget http://gmplib.org/download/gmp/gmp-6.1.1.tar.xz
-    wget http://www.mpfr.org/mpfr-current/mpfr-3.1.4.tar.xz
-    wget http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
+    wget http://ftp.gnu.org/gnu/binutils/$BIN_VER.tar.gz
+    wget http://ftp.gnu.org/gnu/gcc/$GCC_VER/$GCC_VER.tar.gz
+    wget http://gmplib.org/download/gmp/$GMP_VER.tar.xz
+    wget http://www.mpfr.org/mpfr-current/$MPF_VER.tar.xz
+    wget http://ftp.gnu.org/gnu/mpc/$MPC_VER.tar.gz
 
     echo 'Extracting'
-    tar xzf binutils-2.27.tar.gz
-	tar xzf gcc-6.2.0.tar.gz
-	tar xJf gmp-6.1.0.tar.xz
-	tar xJf mpfr-3.1.4.tar.xz
-    tar xzf mpc-1.0.3.tar.gz
+    tar xzf $BIN_VER.tar.gz
+	tar xzf $GCC_VER.tar.gz
+	tar xJf $GMP_VER.tar.xz
+	tar xJf $MPF_VER.tar.xz
+    tar xzf $MPC_VER.tar.gz
 
-    mv gmp-6.2.0 gcc-6.1.0/gmp
-	mv mpfr-3.1.4 gcc-6.1.0/mpfr
-	mv mpc-1.0.3 gcc-6.1.0/mpc
+    mv $GMP_VER $GCC_VER/gmp
+	mv $MPF_VER $GCC_VER/mpfr
+	mv $MPC_VER $GCC_VER/mpc
 
     cd - || exit
 fi
 
 echo 'Building binutils'
 mkdir xcompiler/build/binutils && cd "$_" || exit
-../../src/binutils-2.26/configure --target=$ARCH --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../../src/$BIN_VER/configure --target=$ARCH --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make -j$CCOUNT
 make install
 cd - || exit
 
 echo 'Building GCC'
 mkdir xcompiler/build/gcc && cd "$_" || exit
-../../src/gcc-6.1.0/configure --target=$ARCH --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+../../src/$GCC_VER/configure --target=$ARCH --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc -j$CCOUNT
 make all-target-libgcc -j$CCOUNT
 make install-gcc
