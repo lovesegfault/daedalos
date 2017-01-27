4f952063f6ca114000e70191fe4129cbd2395579
Bernardo Meurer <meurer@daedalos.localdomain>
2017-01-27 02:10:40 +0000

    New cross-compiler built script

--- scripts/new-xcompiler.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ARCH=x86_64-elf
-PREFIX="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-echo "$ARCH"
-echo "$PREFIX"
--- scripts/xcompiler.sh
+++ scripts/xcompiler.sh
@@ -1,82 +1,119 @@
 #!/usr/bin/env bash
 
+dir="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)")"
+
 ARCH=x86_64-elf
-PREFIX=$(pwd)/xcompiler/$ARCH
-export PATH=$PATH:$PREFIX/bin
-export CFLAGS="-g -O2"
-CCOUNT=$(nproc --all)
+PREFIX="$dir/xcompiler"
+workdir="$dir/xcompiler/script"
+PARALLEL="-j$(nproc --all)"
 
 BINUTILS="binutils-2.27"
 GCC="gcc-6.2.0"
 GMP="gmp-6.1.1"
-MPFR="mpfr-3.1.4"
+MPFR="mpfr-3.1.5"
 MPC="mpc-1.0.3"
 
 if [ "$(uname)" == "Darwin" ]; then
-	echo "This doesn't work on MacOS yet!"
+    echo "This does not work on MacOS currently."
     exit
 fi
 
+OPTIONS_BINUTILS="--target=$ARCH --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror"
+OPTIONS_GCC="--target=$ARCH --prefix=$PREFIX --disable-nls --enable-languages=c,c++ --without-headers"
 
+mkdir -p "$PREFIX"
+mkdir -p "$workdir"
 
-rm -rf xcompiler/build
-#rm -rf xcompiler/$ARCH
-mkdir -p xcompiler/build
-mkdir -p xcompiler/$ARCH
+cd $workdir || exit
 
-if [[ ! -d xcompiler/src ]]; then
-    mkdir -p xcompiler/src
-    cd xcompiler/src || exit
-
-    echo 'Downloading Sources'
-    wget http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.gz
-    wget http://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.gz
-    wget http://gmplib.org/download/gmp/$GMP.tar.xz
-    wget http://www.mpfr.org/mpfr-current/$MPFR.tar.xz
-    wget http://ftp.gnu.org/gnu/mpc/$MPC.tar.gz
-
-    echo 'Extracting'
-    tar xzf $BINUTILS.tar.gz
-	tar xzf $GCC.tar.gz
-	tar xJf $GMP.tar.xz
-	tar xJf $MPFR.tar.xz
-    tar xzf $MPC.tar.gz
-
-    mv $GMP $GCC/gmp
-	mv $MPFR $GCC/mpfr
-	mv $MPC $GCC/mpc
-
-    cd - || exit
+echo 'int main() {return 0;}' > test.c
+error=0
+if  ! gcc -lgmp test.c -o test; then
+    error=1
+    echo 'GMP (gmp-devel/libgmp-dev) dependency seems to be missing!'
 fi
-if [[ ! -d xcompiler/$ARCH/bin ]]; then
-	echo 'Building binutils'
-	mkdir xcompiler/build/binutils && cd "$_" || exit
-	../../src/$BINUTILS/configure --target=$ARCH --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-	make -j$CCOUNT
-	make install
-	cd - || exit
-
-	echo 'Building GCC'
-	mkdir xcompiler/build/gcc && cd "$_" || exit
-	../../src/$GCC/configure --target=$ARCH --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-	make all-gcc -j$CCOUNT
-	make all-target-libgcc -j$CCOUNT
-	make install-gcc
-	make install-target-libgcc
-	cd - || exit
-
-	rm -rf xcompiler/build
-else
-	echo "It seems you already have a working Cross Compiler."
+if ! gcc -lmpfr test.c -o test; then
+    error=1
+    echo 'MPFR (mpfr-devel/libmpfr-dev) dependency seems to be missing!'
 fi
+if ! gcc -lmpc test.c -o test; then
+    error=1
+    echo 'MPC (mpc-devel/libmpc-dev) dependency seems to be missing!'
+fi
+if ! gcc -lz test.c -o test; then
+    error=1
+    echo 'zlib (zlib-devel/zlib1g-dev) dependency seems to be missing!'
+fi
+if ! gcc -lpython2.7 test.c -o test; then
+    error=1
+    echo 'libpython2.7 (python-devel/python2.7-dev) dependency seems to be missing!'
+fi
+rm -f test test.c
+if ! makeinfo -h > /dev/null; then
+    error=1
+    echo 'makeinfo (texinfo) dependency seems to be missing!'
+fi
+[ $error -eq 1 ] && exit 1
+
+download=$workdir/download
+build=$workdir/build
+
+mkdir -p $download $build
+
+if [ ! -f .downloaded ]; then
+    wget -c http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2 -O $download/$BINUTILS.tar.bz2 && \
+        tar xvjf $download/$BINUTILS.tar.bz2 && \
+        wget -c ftp://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2 -O $download/$GCC.tar.bz2 && \
+        tar xvjf $download/$GCC.tar.bz2  && \
+        wget -c http://gmplib.org/download/gmp/$GMP.tar.xz -O $download/$GDB.tar.xz && \
+        tar xvJf $download/$GDB.tar.xz && \
+        wget -c http://mpfr.loria.fr/$MPFR/$MPFR.tar.xz -O $download/$MPFR.tar.xz && \
+        tar xvJf $download/$MPFR.tar.xz && \
+        wget -c http://ftp.gnu.org/gnu/mpc/$MPC.tar.gz -O $download/$MPC.tar.gz && \
+        tar xvzf $download/$MPC.tar.gz && \
+        touch .downloaded
+    if [ $? -ne 0 ]; then
+        echo "Download failed!"
+        exit 1
+    fi
+fi
+
+# binutils
+echo "Building binutils"
+if [ ! -f .built_binutils ]; then
+    cd $build || exit 1
+    rm -rf * || exit 1
+    $workdir/$BINUTILS/configure $OPTIONS_BINUTILS || exit 1
+    make $PARALLEL all || exit 1
+    make install || exit 1
+    cd $workdir || exit 1
+    rm -rf ${build:?}/* || exit 1
+    touch .built_binutils || exit 1
+fi
+
+# GCC
+echo "Building GCC"
+
+if [ ! -f .built_gcc ]; then
+    cd $build || exit 1
+    rm -rf * || exit 1
+    $workdir/$GCC/configure $OPTIONS_GCC || exit 1
+    make $PARALLEL all-gcc || exit 1
+    make $PARALLEL all-target-libgcc || exit 1
+    make install-gcc || exit 1
+    make install-target-libgcc || exit 1
+    cd $workdir || exit 1
+    rm -rf ${build:?}/* || exit 1
+    touch .built_gcc || exit 1
+fi
+
 if [ $SHELL == "/usr/bin/bash" ]||[ $SHELL == "/bin/bash" ];then
-	echo "It seems like you're using bash. To add the binaries to your PATH run"
-	echo 'export PATH=$PATH':$PREFIX/bin
+    echo "It seems like you're using bash. To add the binaries to your PATH run"
+    echo 'export PATH=$PATH':$PREFIX/bin
 fi
 if [ $SHELL == "/usr/bin/fish" ];then
-	echo "It seems like you're using fish. To add the binaries to your PATH run"
-	echo 'set -gx PATH $PATH' $PREFIX/bin
+    echo "It seems like you're using fish. To add the binaries to your PATH run"
+    echo 'set -gx PATH $PATH' $PREFIX/bin
 fi
-
 
 echo 'Done'
