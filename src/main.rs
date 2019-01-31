c33eb3eb3a01544335551b4164e9c511afd6fabe
Bernardo Meurer <meurerbernardo@gmail.com>
2019-01-31 04:57:39 -0800

    Reorganized main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
 use daedalos::println;
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, world!");
+    loop {}
+}
+
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
