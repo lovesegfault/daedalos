9eeb4b14ad069c2c5ca8c89ff299264e37b706de
Bernardo Meurer <meurerbernardo@gmail.com>
2019-01-31 02:44:45 -0800

    Added proper main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -7,6 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-
-fn main() {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
