ed15020e4fc254fdd1d7a1c47a402bac61dad9ee
Bernardo Meurer <meurerbernardo@gmail.com>
2019-07-17 20:00:48 -0700

    Organize panic handler test

--- tests/panic_handler.rs
+++ tests/panic_handler.rs
@@ -1,3 +1,5 @@
+#![no_std]
+#![no_main]
 #![feature(panic_info_message)]
 
 use core::fmt::{self, Write};
@@ -5,19 +7,46 @@ use daedalos::qemu::{exit_qemu, QemuExitCode};
 use daedalos::{sprint, sprintln};
 
 const MESSAGE: &str = "Example panic message from panic_handler test";
-const PANIC_LINE: u32 = 14; // adjust this when moving the `panic!` call
+const PANIC_LINE: u32 = 15; // adjust this when moving the `panic!` call
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    sprint!("panic_handler... ");
+    serial_print!("panic_handler... ");
     panic!(MESSAGE); // must be in line `PANIC_LINE`
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    check_location(info);
+    check_message(info);
+
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+fn fail(error: &str) -> ! {
+    serial_println!("[failed]");
+    serial_println!("{}", error);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+fn check_location(info: &PanicInfo) {
+    let location = info.location().unwrap_or_else(|| fail("no location"));
+    if location.file() != file!() {
+        fail("file name wrong");
+    }
+    if location.line() != PANIC_LINE {
+        fail("file line wrong");
+    }
 }
 
 fn check_message(info: &PanicInfo) {
     let message = info.message().unwrap_or_else(|| fail("no message"));
     let mut compare_message = CompareMessage { expected: MESSAGE };
     write!(&mut compare_message, "{}", message).unwrap_or_else(|_| fail("write failed"));
-    if !compare_message.expected.is_empty() {
+    if compare_message.expected.len() != 0 {
         fail("message shorter than expected message");
     }
 }
@@ -40,30 +69,4 @@ impl fmt::Write for CompareMessage {
         }
         Ok(())
     }
-}
-
-fn fail(error: &str) -> ! {
-    serial_println!("[failed]");
-    serial_println!("{}", error);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-fn check_location(info: &PanicInfo) {
-    let location = info.location().unwrap_or_else(|| fail("no location"));
-    if location.file() != file!() {
-        fail("file name wrong");
-    }
-    if location.line() != PANIC_LINE {
-        fail("file line wrong");
-    }
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    check_message(info);
-    check_location(info);
-    serial_println!("[ok]");
-    exit_qemu(QemuExitCode::Success);
-    loop {}
 }
