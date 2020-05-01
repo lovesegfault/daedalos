95616dbbde070097df40370099a4b55bc6cf4ac5
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-01 14:22:44 -0700

    vga: init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -11,5 +13,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    static HELLO: &[u8] = b"Hello, world!";
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
---
+++ src/vga.rs
@@ -0,0 +1,1 @@
+pub const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
