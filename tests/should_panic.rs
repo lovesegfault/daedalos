c85e9142597f6b89bc8f86e0a862372b05a8301d
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-01 18:30:11 -0700

    tests/should_panic: nuke the runner

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
-#![feature(custom_test_frameworks)]
 #![no_main]
 #![no_std]
-#![reexport_test_harness_main = "test_main"]
-#![test_runner(test_runner)]
 
 use daedalos::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
@@ -13,24 +10,14 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
-
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_fail... ");
     assert_eq!(0, 1);
