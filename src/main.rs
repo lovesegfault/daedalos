3a6b0982d3534e86c005221a93ed82453ed55b8a
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-02 13:44:46 -0700

    memory: use mapper

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,15 @@ use daedalos::println;
 bootloader::entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static bootloader::BootInfo) -> ! {
-    use daedalos::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use daedalos::memory;
+    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
 
     println!("Hello World{}", "!");
 
     daedalos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
     let addresses = [
         // the identity-mapped vga buffer page
@@ -31,7 +32,8 @@ fn kernel_main(boot_info: &'static bootloader::BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        // new: use the `mapper.translate_addr` method
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,19 @@
-use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
+use x86_64::{
+    structures::paging::{OffsetPageTable, PageTable},
+    PhysAddr,
+    VirtAddr,
+};
+
+// Initialize a new OffsetPageTable.
+//
+// This function is unsafe because the caller must guarantee that the
+// complete physical memory is mapped to virtual memory at the passed
+// `physical_memory_offset`. Also, this function must be only called once
+// to avoid aliasing `&mut` references (which is undefined behavior).
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
 
 // Returns a mutable reference to the active level 4 table.
 //
@@ -6,7 +21,7 @@ use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
 // complete physical memory is mapped to virtual memory at the passed
 // `physical_memory_offset`. Also, this function must be only called once
 // to avoid aliasing `&mut` references (which is undefined behavior).
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
@@ -16,49 +31,4 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // unsafe
-}
-
-// Translates the given virtual address to the mapped physical address, or
-// `None` if the address is not mapped.
-//
-// This function is unsafe because the caller must guarantee that the
-// complete physical memory is mapped to virtual memory at the passed
-// `physical_memory_offset`.
-pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
-    translate_addr_inner(addr, physical_memory_offset)
-}
-
-/// only be reachable through `unsafe fn` from outside of this module.
-fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
-    use x86_64::{registers::control::Cr3, structures::paging::page_table::FrameError};
-
-    // read the active level 4 frame from the CR3 register
-    let (level_4_table_frame, _) = Cr3::read();
-
-    let table_indexes = [
-        addr.p4_index(),
-        addr.p3_index(),
-        addr.p2_index(),
-        addr.p1_index(),
-    ];
-    let mut frame = level_4_table_frame;
-
-    // traverse the multi-level page table
-    for &index in &table_indexes {
-        // convert the frame into a page table reference
-        let virt = physical_memory_offset + frame.start_address().as_u64();
-        let table_ptr: *const PageTable = virt.as_ptr();
-        let table = unsafe { &*table_ptr };
-
-        // read the page table entry and update `frame`
-        let entry = &table[index];
-        frame = match entry.frame() {
-            Ok(frame) => frame,
-            Err(FrameError::FrameNotPresent) => return None,
-            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
-        };
-    }
-
-    // calculate the physical address by adding the page offset
-    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
