9b48ef1c3a65536eed661ca7e59e177454dc213f
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-01 17:13:12 -0700

    vga: implement print,println

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
     loop {}
 }
--- src/vga.rs
+++ src/vga.rs
@@ -125,3 +125,20 @@ lazy_static! {
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
