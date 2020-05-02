982d9e10222f9b55f7bcb2c8da6abed92eb28c46
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-01 22:25:17 -0700

    tests/stack_overflow: init

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,6 @@ pub extern "C" fn _start() -> ! {
 
     daedalos::init();
 
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
---
+++ tests/stack_overflow.rs
@@ -0,0 +1,57 @@
+#![feature(abi_x86_interrupt)]
+#![no_main]
+#![no_std]
+
+use daedalos::{exit_qemu, serial_println, QemuExitCode};
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::InterruptStackFrame;
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(daedalos::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+pub fn init_test_idt() {
+    TEST_IDT.load();
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    daedalos::test_panic_handler(info)
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    daedalos::serial_print!("stack_overflow... ");
+
+    daedalos::gdt::init();
+    init_test_idt();
+
+    // trigger a stack overflow
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+}
