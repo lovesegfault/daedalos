a47c0bbbcbf0ff2969d14172270903cb11d15a08
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-02 18:50:23 -0700

    task/simple_executor: init

---
+++ src/task/simple_executor.rs
@@ -0,0 +1,16 @@
+use super::Task;
+use alloc::collections::VecDeque;
+
+pub struct SimpleExecutor {
+    task_queue: VecDeque<Task>,
+}
+
+impl SimpleExecutor {
+    pub fn new() -> SimpleExecutor {
+        SimpleExecutor {
+            task_queue: VecDeque::new(),
+        }
+    }
+
+    pub fn spawn(&mut self, task: Task) { self.task_queue.push_back(task) }
+}
