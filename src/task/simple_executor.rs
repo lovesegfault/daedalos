87642b4e458c5b59cc9f8675493eec67aabe3d58
Bernardo Meurer <meurerbernardo@gmail.com>
2020-05-03 09:49:39 -0700

    task/simple_executor: dummy waker

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,5 +1,6 @@
-use super::Task;
+use crate::task::Task;
 use alloc::collections::VecDeque;
+use core::task::{RawWaker, RawWakerVTable, Waker};
 
 pub struct SimpleExecutor {
     task_queue: VecDeque<Task>,
@@ -14,3 +15,13 @@ impl SimpleExecutor {
 
     pub fn spawn(&mut self, task: Task) { self.task_queue.push_back(task) }
 }
+
+fn dummy_raw_waker() -> RawWaker {
+    fn no_op(_: *const ()) {}
+    fn clone(_: *const ()) -> RawWaker { dummy_raw_waker() }
+
+    let vtable = &RawWakerVTable::new(clone, no_op, no_op, no_op);
+    RawWaker::new(0 as *const (), vtable)
+}
+
+fn dummy_waker() -> Waker { unsafe { Waker::from_raw(dummy_raw_waker()) } }
